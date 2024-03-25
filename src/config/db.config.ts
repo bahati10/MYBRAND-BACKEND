9839f91ddb626a920684c338aaec029dc1e3a215
Bahati Yves <bahatiyves10@gmail.com>
2024-03-25 19:55:51 +0200

    Initial commit

---
+++ src/config/db.config.ts
@@ -0,0 +1,18 @@
+//importing modules
+import mongoose from 'mongoose'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectionString = process.env.DB_CONN_STRING;
+
+//db connection
+export const db = mongoose
+  .connect(connectionString || "DB_CONN_STRING is not defined.")
+  .then(() => {
+    console.log(`Database connection successful to ${process.env.DB_NAME}`);
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+    process.exit(1);
+  });
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import { db } from './config/db.config.js'
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const app = express()
+const port = process.env.PORT
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+//routes
+// app.use("/api", todosRouter, usersRouter)
+
+//db connection then server connection
+db.then(() => {
+    app.listen(port, () => console.log(`Server started at http://localhost:${port}`))
+})
