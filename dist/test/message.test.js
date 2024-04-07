922307765edbb0a2b82891967c8e29e743497620
Bahati Yves <bahatiyves10@gmail.com>
2024-04-07 15:39:12 +0200

    Circle CI setup

--- dist/test/message.test.js
+++ dist/test/message.test.js
@@ -3,7 +3,8 @@ import request from 'supertest';
 import { app } from '../index.js';
 let authTokenAdmin;
 let authTokenUser;
-before(async () => {
+before(async function () {
+    this.timeout(10000);
     // Login as admin
     const adminLoginRes = await request(app)
         .post('/api/admin/login')
@@ -59,6 +60,44 @@ describe('MessageController', () => {
                 .set('Authorization', `Bearer ${authTokenAdmin}`);
             expect(res.status).to.equal(200);
             expect(res.body).to.have.property('message').that.includes('Message Deleted successfully');
+        });
+    });
+});
+describe('BlogController', () => {
+    describe('addComment', () => {
+        it('should add a comment to a blog successfully', async () => {
+            const res = await request(app)
+                .post('/api/blog/66056563021ca1042671ada8/comment')
+                .send({ content: 'Test Comment Content' })
+                .set('Authorization', `Bearer ${authTokenUser}`);
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property('message').that.includes('Comment added successfully');
+        });
+    });
+    describe('likeBlog', () => {
+        it('should like a blog successfully', async () => {
+            const res = await request(app)
+                .post('/api/blog/66056563021ca1042671ada8/like')
+                .set('Authorization', `Bearer ${authTokenUser}`);
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property('message').that.includes('Blog liked successfully');
+        });
+    });
+    describe('addPost', () => {
+        it('should add a post successfully', async () => {
+            const messageData = {
+                image: 'Test content.url',
+                title: 'Test contentTest content',
+                subtitle: 'Test contentTest contentTest content',
+                content: 'Test content',
+            };
+            const res = await request(app)
+                .post('/api/blog/addblog')
+                .send(messageData)
+                .set('Authorization', `Bearer ${authTokenAdmin}`);
+            expect(res.status).to.equal(201);
+            expect(res.body).to.have.property('message').that.includes('Blog Added successfully');
+            expect(res.body).to.have.property('message');
         });
     });
 });
