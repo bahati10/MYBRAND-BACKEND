658d3db7770a2346ce11406ab0cab1f2d54d5407
Bahati Yves <bahatiyves10@gmail.com>
2024-04-07 15:39:12 +0200

    Circle CI setup

--- dist/test/user.test.js
+++ dist/test/user.test.js
@@ -1,6 +1,21 @@
 import { expect } from 'chai';
 import request from 'supertest';
 import { app } from '../index.js';
+let authTokenAdmin;
+let authTokenUser;
+before(async function () {
+    this.timeout(10000);
+    // Login as admin
+    const adminLoginRes = await request(app)
+        .post('/api/admin/login')
+        .send({ email: 'admin@gmail.com', password: 'admin123' });
+    authTokenAdmin = adminLoginRes.body.token;
+    // Login as user
+    const userLoginRes = await request(app)
+        .post('/api/login')
+        .send({ email: 'testuser@gmail.com', password: 'testuser123' });
+    authTokenUser = userLoginRes.body.token;
+});
 describe('UserController', () => {
     // describe('adduser', () => {
     //     it('should add a new user', async () => {
@@ -9,7 +24,7 @@ describe('UserController', () => {
     //             .send({
     //                 firstname: 'John',
     //                 lastname: 'Doe',
-    //                 email: 'john.doe1@example.com',
+    //                 email: 'johndoe1111@example.com',
     //                 password: 'password123',
     //             });
     //         expect(res.status).to.equal(201);
@@ -37,6 +52,34 @@ describe('UserController', () => {
             });
             expect(res.status).to.equal(401);
             expect(res.body).to.have.property('message').that.is.a('string');
+        });
+        describe('getUsers', () => {
+            it('should get all users successfully', async () => {
+                const res = await request(app)
+                    .get('/api/users')
+                    .set('Authorization', `Bearer ${authTokenAdmin}`);
+                expect(res.status).to.equal(200);
+                expect(res.body).to.have.property('message').that.includes('Users retrieved successfully');
+            });
+        });
+        describe('getUser', () => {
+            it('should get user successfully', async () => {
+                const res = await request(app)
+                    .get('/api/users/660be6cf7b46315326437299')
+                    .set('Authorization', `Bearer ${authTokenAdmin}`);
+                expect(res.status).to.equal(200);
+                expect(res.body).to.have.property('message').that.includes('User retrieved');
+                expect(res.body).to.have.property('message');
+            });
+        });
+        describe('deleteMessage', () => {
+            it('should delete user successfully', async () => {
+                const res = await request(app)
+                    .delete('/api/user/delete/660d4a37b9541096deefbf01')
+                    .set('Authorization', `Bearer ${authTokenAdmin}`);
+                expect(res.status).to.equal(200);
+                expect(res.body).to.have.property('message').that.includes('User deleted successfully');
+            });
         });
     });
 });
--- src/test/message.test.ts
+++ src/test/message.test.ts
@@ -5,7 +5,9 @@ import { app } from '../index.js';
 let authTokenAdmin: string;
 let authTokenUser: string;
 
-before(async () => {
+before(async function() {
+
+  this.timeout(10000);
   // Login as admin
   const adminLoginRes = await request(app)
     .post('/api/admin/login')
@@ -74,3 +76,52 @@ describe('MessageController', () => {
     });
   });
 });
+
+describe('BlogController', () => {
+
+  describe('addComment', () => {
+    it('should add a comment to a blog successfully', async () => {
+      const res = await request(app)
+        .post('/api/blog/66056563021ca1042671ada8/comment')
+        .send({ content: 'Test Comment Content' })
+        .set('Authorization', `Bearer ${authTokenUser}`);
+
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property('message').that.includes('Comment added successfully');
+    });
+  });
+
+  describe('likeBlog', () => {
+    it('should like a blog successfully', async () => {
+      const res = await request(app)
+        .post('/api/blog/66056563021ca1042671ada8/like')
+        .set('Authorization', `Bearer ${authTokenUser}`);
+
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property('message').that.includes('Blog liked successfully');
+    });
+  });
+
+  describe('addPost', () => {
+    it('should add a post successfully', async () => {
+      const messageData = {
+        image: 'Test content.url',
+        title: 'Test contentTest content',
+        subtitle: 'Test contentTest contentTest content',
+        content: 'Test content',
+      };
+
+      const res = await request(app)
+        .post('/api/blog/addblog')
+        .send(messageData)
+        .set('Authorization', `Bearer ${authTokenAdmin}`);
+
+      expect(res.status).to.equal(201);
+      expect(res.body).to.have.property('message').that.includes('Blog Added successfully');
+      expect(res.body).to.have.property('message');
+    });
+  });
+});
+
+
+
