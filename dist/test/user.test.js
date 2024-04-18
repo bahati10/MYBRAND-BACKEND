96a7ec179ec4145582137a8eacedeb7be29baf3e
Bahati Yves <bahatiyves10@gmail.com>
2024-04-18 13:15:18 +0200

    Fixing CORS configurations

--- dist/test/user.test.js
+++ dist/test/user.test.js
@@ -16,6 +16,9 @@ before(async function () {
         .send({ email: 'testuser@gmail.com', password: 'testuser123' });
     authTokenUser = userLoginRes.body.token;
 });
+after(function () {
+    process.exit();
+});
 describe('UserController', () => {
     // describe('adduser', () => {
     //     it('should add a new user', async () => {
